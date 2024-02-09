de14da17508e1c80f4a8a107bc77b23e6bb398bc
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2024-02-09 10:35:19 +0100

    Future<void> instead of Future<Position>

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -5,13 +5,12 @@ class Location {
   double latitude = 0.0;
   double longitude = 0.0;
 
-  Future<Position> getCurrentPosition() async{
+  Future<void> getCurrentPosition() async{
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.low);
       latitude = position.latitude;
       longitude = position.longitude;
-      return position;
     }
     catch (e) {
       throw e;
